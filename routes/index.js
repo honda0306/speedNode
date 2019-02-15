dd77b1f21a800a570fd61afca6467e817c912d57
Chris Honda <christopher.honda92@gmail.com>
2019-02-15 14:46:03 -0800

    Experimenting with req/res for familiarity

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const honda = { name: 'Chris', age: '500', cool: false };
+  res.send(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;

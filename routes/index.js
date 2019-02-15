a21fe16cc54cce8edc0440365cac57213109bb1f
Chris Honda <christopher.honda92@gmail.com>
2019-02-15 15:43:51 -0800

    Added two pug files after intro; started controller for store

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const honda = { name: 'Chris', age: '500', cool: false };
-  res.send(req.query);
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');

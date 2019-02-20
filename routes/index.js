f8b27bffe1a023f40c5ed54596b4bdfeb12ec919
Chris Honda <christopher.honda92@gmail.com>
2019-02-20 09:03:54 -0800

    Refresher on middleware, Create store schema

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,4 @@ const storeController = require('../controllers/storeController');
 // Do work here
 router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
-
 module.exports = router;
